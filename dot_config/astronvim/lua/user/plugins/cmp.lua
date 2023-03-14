d05cf34ff58ed65219557c23695b36747957a632
Spencer Ellinor <zpencerq@users.noreply.github.com>
2023-03-14 14:53:02 -0400

    update cmp and use default keybinds

--- dot_config/astronvim/lua/user/plugins/cmp.lua
+++ dot_config/astronvim/lua/user/plugins/cmp.lua
@@ -1,28 +1,21 @@
 return {
   "hrsh7th/nvim-cmp",
   dependencies = {
+    "saadparwaiz1/cmp_luasnip",
+    "hrsh7th/cmp-buffer",
     "hrsh7th/cmp-calc",
     "hrsh7th/cmp-emoji",
-    "jc-doyle/cmp-pandoc-references",
-    "kdheepak/cmp-latex-symbols",
+    "hrsh7th/cmp-path",
+    "hrsh7th/cmp-nvim-lsp",
   },
   opts = function(_, opts)
     local cmp = require "cmp"
-    local luasnip = require "luasnip"
-
-    local function next_item()
-      if cmp.visible() then
-        cmp.select_next_item { behavior = cmp.SelectBehavior.Insert }
-      else
-        cmp.complete()
-      end
-    end
 
     return require("astronvim.utils").extend_tbl(opts, {
       window = {
         completion = {
           winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,CursorLine:Visual,Search:None",
-          border = "none",
+          border = "single",
           col_offset = -1,
           side_padding = 0,
         },
@@ -30,30 +23,10 @@ return {
       sources = cmp.config.sources {
         { name = "nvim_lsp", priority = 1000 },
         { name = "luasnip", priority = 750 },
-        { name = "pandoc_references", priority = 725 },
-        { name = "latex_symbols", priority = 700 },
         { name = "emoji", priority = 700 },
         { name = "calc", priority = 650 },
         { name = "path", priority = 500 },
         { name = "buffer", priority = 250 },
-      },
-      mapping = {
-        ["<C-n>"] = next_item,
-        ["<C-j>"] = next_item,
-        ["<Tab>"] = cmp.mapping(function(fallback)
-          if luasnip.jumpable(1) then
-            luasnip.jump(1)
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
-          if luasnip.jumpable(-1) then
-            luasnip.jump(-1)
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
       },
     })
   end,
